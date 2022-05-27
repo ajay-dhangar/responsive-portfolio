e7627697260e5c21d30c648bbdaa6d07c4bfdbbd
Ajay Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2022-05-27 23:39:32 +0530

    Done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /*==================== typing animation =========================== */
 var typed = new Typed(".typing", {
-    strings: ["A Web Developer", "A Web Designer", "An App Developer", "A You Tube"],
+    strings: ["A Web Developer", "A Web Designer", "An App Developer", "A You Tuber"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
